1c0900f252ff6dccfb618e0c9269924560f49a3a
Elisaveta <89607033+elisaveta9@users.noreply.github.com>
2023-04-27 15:47:19 +0300

    Update FirstTask.cs

--- FirstTask.cs
+++ FirstTask.cs
@@ -1,22 +1,39 @@
 namespace Library
 {
-    public class ZadanieOne
-    {
-        public void One()
+    public static class PrintTasks //должны быть значащие названия. ни в одном из классов не функции Main() - значит эти классы не главные
+    {                       //эти классы можно объединить. Обе задачи используются для вывода в консоль, поэтому так назван класс
+                            //В классе нет полей, значит лучше его сделать статическим, как и его функции.
+        public static void PrintSequence() //функция для вывода последовательности 1, 2, ..., n
         {
-            Console.Write("Введите число N: ");
-            int n = int.Parse(Console.ReadLine());
+            Console.Write("Вывод последовательности от 1 до N. Введите число N: ");
+            if (!int.TryParse(Console.ReadLine(), out int n))   //инициализируем n без получения ошибки
+            {
+                Console.WriteLine("Введенное значение невозможно считать как целочисленную переменную."); 
+                return;
+            }
+            if (n < 1)  //проверяем корректность заданного значения
+            {
+                Console.WriteLine("Число N должно быть больше либо равно 1.");
+                return;
+            }
             string result = string.Join(", ", Enumerable.Range(1, n));
             Console.WriteLine(result);
         }
-    }
-    public class ZadanieTwo
-    {
-        public static void Two()
+        
+        public static void PrintSquare() //функция для вывода на экран квадрата
         {
-            Console.Write("Введите нечетное число N: ");
-            int n = int.Parse(Console.ReadLine());
-            if (n % 2 == 0) // If N is an even number, exit the function.
+            Console.Write("Печать квадрата NxN с «дыркой» в центре. Введите нечетное число N больше 3: ");
+            if (!int.TryParse(Console.ReadLine(), out int n))   //инициализируем n без получения ошибки
+            {
+                Console.WriteLine("Введенное значение невозможно считать как целочисленную переменную."); 
+                return;
+            }
+            if (n < 3)  //проверки на корректность введенного значения
+            {
+                Console.WriteLine("Не выполнено условие N > 3.");
+                return;
+            }
+            if (n % 2 == 0) // 
             {
                 Console.WriteLine("Вы ввели четное N.");
                 return;
@@ -27,6 +44,34 @@ namespace Library
                     else Console.Write("#");                    
                 }
                 Console.WriteLine();
+            }
+        }
+    }
+    public class Program
+    {
+        private static void Main() //примерное использование описанных функций
+        {                          
+            Console.WriteLine(@"Выбор функции для выполнения.
+1. Вывод последовательности от 1 до n.
+2. Вывод квадрата с дыркой в центре.
+Любой другой вариант - выход из программы");
+            if (!int.TryParse(Console.ReadLine(), out int n))   //инициализируем n без получения ошибки
+            {
+                Console.WriteLine("Введенное значение невозможно считать как целочисленную переменную."); 
+                return;
+            }
+            switch (n)
+            {
+                case 1:
+                    PrintTasks.PrintSequence();
+                    break;
+                case 2:
+                    
+                    PrintTasks.PrintSequence();
+                    break;
+                default:
+                    Console.WriteLine("Выход из программы...");
+                    break;
             }
         }
     }
